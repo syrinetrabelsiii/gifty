1fb1ce33ed1315a81215b6cacf91660f32e1fe38
syrinetrabelsiii <syrinetrabelsi99@gmail.com>
2023-12-26 15:02:49 +0100

    middlwares

--- backend/middlwares/errorHandler.js
+++ backend/middlwares/errorHandler.js
@@ -11,6 +11,7 @@ const errorHandler = (err, req, res, next) => {
     const statuscode = res.statusCode == 200 ? 500 : res.statusCode;
     res.status(statuscode);
     res.json({
+      status: "fail",
       message: err?.message,
       stack: err?.stack,
     });
